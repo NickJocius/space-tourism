851410573e6b50a47bb0645280bb21e13558b7a6
Nick Jocius <capacity512@yahoo.com>
2021-11-19 14:42:45 -0600

    lazy loading views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {lazy, Suspense} from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+// Views
+const Home = lazy(() => import('./views/Home'));
+
 function App() {
   return (
     <Suspense fallback={
-
+      <div>
+        <h1>Loading...</h1>
+      </div>
     }>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+      </Routes>
     </Suspense>
   );
 }
