6aac26c75e2cd9b7df1ba750dbfcbdef76f2b164
Nick Jocius <capacity512@yahoo.com>
2021-11-29 12:42:03 -0600

    starting responsive css

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -26,11 +26,11 @@ const TopNav = () => {
     return (
         
         <nav className={`container min-w-full flex absolute z-40 h-24 mt-6`}>
-            <div className={`flex items-center flex-row pl-6`}>
+            <div className={`flex items-center flex-row pl-6 w-2/5`}>
                 <div className={`inline h-12 w-12`}>
                 <img src={logo} alt="Logo"/>
                 </div>
-                <div className={`w-96 h-px  opacity-20 bg-white -ml-2`}></div>
+                <div className={` h-px w-full opacity-20 bg-white -mr-4 pl-6 z-10`}></div>
             </div>
             <div className={`flex-auto `}>
                 <div className={`h-full flex justify-evenly items-center bg-trans backdrop-filter backdrop-blur`}>
--- src/views/Home.js
+++ src/views/Home.js
@@ -4,10 +4,10 @@ import Intro from '../components/Home/Intro'
 import Explore from '../components/Home/Explore'
 const Home = () => {
     return (
-        <main className={`Home container bg-primary min-h-screen min-w-full relative `}>
+        <main className={`Home container bg-primary min-h-screen max-w-full relative `}>
             <TopNav/>
             <div className={`container min-w-full min-h-screen bg-homeDesk bg-no-repeat bg-cover`} id="bitMap">
-                <div className={`grid grid-cols-2 gap-4 min-h-full min-w-full`}>
+                <div className={`grid grid-cols-1 md:grid-cols-2 gap-4 min-h-full min-w-full`}>
                     <Intro />
                     <Explore/>
                 </div>
