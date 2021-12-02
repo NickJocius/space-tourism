2a938cb614dc786ecb055d3bea5bbf2703eaa844
Nick Jocius <capacity512@yahoo.com>
2021-12-02 10:35:01 -0600

    adjusted bitmap dimensions for responsive behavior

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,7 @@ import Explore from '../components/Home/Explore'
 const Home = () => {
     return (
             <div className={`container min-w-full min-h-screen bg-homeMob md:bg-homeTab lg:bg-homeDesk bg-no-repeat bg-cover`} id="bitMap">
-                <div className={`grid grid-cols-1 md:grid-cols-2 gap-4 min-h-full min-w-full`}>
+                <div className={`grid grid-cols-1 md:grid-cols-2 min-h-full min-w-full`}>
                     <Intro />
                     <Explore/>
                 </div>
